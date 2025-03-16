9e999187e3d3ed718d3dd66689b3bf875f974144
Sueluck1 <136314227+Sueluck1@users.noreply.github.com>
2025-03-16 21:41:19 +0700

    Update Program.cs

--- HaitenWebAPI/Program.cs
+++ HaitenWebAPI/Program.cs
@@ -15,7 +15,14 @@ builder.Services.AddScoped<IReadingHistoryRepository, ReadingHistoryRepository>(
 builder.Services.AddScoped<IUserMangaListRepository, UserMangaListRepository>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 
-
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(8080);  // Cổng HTTP
+    options.ListenAnyIP(443, listenOptions =>  // Cổng HTTPS
+    {
+        listenOptions.UseHttps();
+    });
+});
 
 
 // Add AutoMapper
