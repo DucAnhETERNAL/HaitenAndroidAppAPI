3df25b653a05df00e4ebf72e5a919f27aca5f3a0
gametmoi123 <duynbde170797@fpt.edu.vn>
2025-03-31 20:36:57 +0700

    ChapterImagesController

--- HaitenWebAPI/Program.cs
+++ HaitenWebAPI/Program.cs
@@ -1,3 +1,10 @@
+using BussinessObject;
+using DataAccess;
+using Microsoft.EntityFrameworkCore;
+using Repository;
+using Repository.Repo;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +13,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<PRMDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+builder.Services.AddScoped<ChapterImagesDAO>();
+builder.Services.AddScoped<IChapterImagesRepository, ChapterImagesRepository>();
+
+
 
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
