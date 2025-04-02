127a55855860ce7b7454469af615bb2dffd5ecc6
Anh.D.Nguyen <ffffffjj7@gmail.com>
2025-04-02 11:12:42 +0700

    ControllerView

---
+++ HaitenWebClient/Controllers/MangaViewDetail.cs
@@ -0,0 +1,23 @@
+﻿using HaitenWebClient.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace HaitenWebClient.Controllers
+{
+    public class Manga : Controller
+    {
+        public IActionResult MangaDetails()
+        {
+            return View();
+        }
+        public IActionResult ReaddingMangaText()
+        {
+            return View();
+        }
+        public IActionResult ListMangaImage()
+        {
+            return View();
+        }
+
+    }
+}
