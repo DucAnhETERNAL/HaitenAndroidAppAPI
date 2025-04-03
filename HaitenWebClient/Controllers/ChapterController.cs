78f7597ed3fc249e21febe21de90f4fbec6b366e
gametmoi123 <duynbde170797@fpt.edu.vn>
2025-04-03 10:47:57 +0700

    MangaImage_Chapter_Image

---
+++ HaitenWebClient/Controllers/ChapterController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HaitenWebClient.Controllers
+{
+    public class ChapterController : Controller
+    {
+        private readonly string _apiBaseUrl = "https://localhost:7016/api/chapter";
+
+        public IActionResult Index(int? mangaId)
+        {
+            // Truyền mangaId vào ViewData
+            ViewData["MangaId"] = mangaId;
+
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
---
+++ HaitenWebClient/Models/ChapterViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace HaitenWebClient.Models
+{
+    public class ChapterViewModel
+    {
+        public int Id { get; set; }
+        public int MangaId { get; set; }
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public int ViewCount { get; set; }
+    }
+}
